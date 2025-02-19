c6b6017146ab965f71ace41808ff0153c18b230e
iamneyk <iamneyk@gmail.com>
2025-02-20 02:45:36 +0700

    add tooltip

--- src/components/Sidebar/nav-settings.tsx
+++ src/components/Sidebar/nav-settings.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SquareCheck, SquareX } from "lucide-react";
+import { CloudOff, SquareCheck, SquareX } from "lucide-react";
 import {
   SidebarGroup,
   SidebarGroupLabel,
@@ -11,11 +11,25 @@ import {
 import React from "react";
 import { ThemeCustomizer } from "../Theme/theme-customizer";
 import { ContentCustomizer } from "../Theme/content-customizer";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export function NavSettings() {
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Tuỳ chỉnh</SidebarGroupLabel>
+      <SidebarGroupLabel className="gap-2">
+        <span>Tuỳ chỉnh</span>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <CloudOff size={18} className="cursor-pointer" />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>
+              Những tuỳ chỉnh này chỉ có hiệu lực trên thiết bị hiện tại, không
+              đồng bộ theo tài khoản!
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </SidebarGroupLabel>
       <SidebarMenu>
         <SidebarMenuItem>
           <ThemeCustomizer />
