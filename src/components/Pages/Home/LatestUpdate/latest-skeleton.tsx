da1a01073e92bddc2fc25122d3f942987fabab92
iamneyk <iamneyk@gmail.com>
2025-02-24 20:08:55 +0700

    smol

--- src/components/Pages/Home/LatestUpdate/latest-skeleton.tsx
+++ src/components/Pages/Home/LatestUpdate/latest-skeleton.tsx
@@ -8,7 +8,7 @@ export default function LatestSkeleton() {
     <Card className="rounded-sm shadow-sm transition-colors duration-200">
       <CardContent className="flex gap-2 p-1">
         <Skeleton className="w-20 h-28 shrink-0 bg-gray-500 rounded-sm" />
-        <div className="flex flex-col justify-evenly w-full">
+        <div className="flex flex-col justify-evenly w-full pr-1">
           <Skeleton className="w-full h-5 bg-gray-500 rounded-sm" />
           <Skeleton className="w-2/3 h-4 bg-gray-500 rounded-sm" />
           <Skeleton className="w-1/3 h-3 bg-gray-500 rounded-sm" />
