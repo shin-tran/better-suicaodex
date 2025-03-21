691c8adfbb7089f0dace1867214dc69c95b3a589
iamneyk <iamneyk@gmail.com>
2025-03-21 22:27:48 +0700

    update url

--- src/components/Pages/History/history-cover-card.tsx
+++ src/components/Pages/History/history-cover-card.tsx
@@ -24,34 +24,46 @@ export default function HistoryCoverCard({ chapter }: HistoryCoverCardProps) {
   const title = ChapterTitle(chapter);
   return (
     <Card className="relative rounded-sm shadow-none transition-colors duration-200 w-full h-full border-none bg-background">
-      <CardContent className="relative p-0 rounded-sm">
-        <LazyLoadImage
-          wrapperClassName={cn(
-            "!block rounded-sm object-cover w-full h-full",
-            !loaded && "aspect-[5/7]"
-          )}
-          placeholderSrc="/images/xidoco.webp"
-          className={cn(
-            "h-auto w-full rounded-sm block object-cover aspect-[5/7]"
-          )}
-          src={src}
-          alt={`Ảnh bìa ${chapter.manga.title}`}
-          onLoad={() => setLoaded(true)}
-          onError={(e) => {
-            e.currentTarget.src = "/images/xidoco.webp";
-          }}
-        />
-      </CardContent>
+      <Link href={`/manga/${chapter.manga.id}`}>
+        <CardContent className="relative p-0 rounded-sm">
+          <LazyLoadImage
+            wrapperClassName={cn(
+              "!block rounded-sm object-cover w-full h-full",
+              !loaded && "aspect-[5/7]"
+            )}
+            placeholderSrc="/images/xidoco.webp"
+            className={cn(
+              "h-auto w-full rounded-sm block object-cover aspect-[5/7]"
+            )}
+            src={src}
+            alt={`Ảnh bìa ${chapter.manga.title}`}
+            onLoad={() => setLoaded(true)}
+            onError={(e) => {
+              e.currentTarget.src = "/images/xidoco.webp";
+            }}
+          />
+        </CardContent>
+      </Link>
 
       <CardFooter className="py-2 px-0 w-full flex flex-col gap-1 items-start">
-        <Link href={`/manga/${chapter.manga.id}`} className="font-bold line-clamp-2 break-all">{chapter.manga.title}</Link>
-        <Link href={`/chapter/${chapter.id}`} className="items-center flex gap-1">
+        <Link
+          href={`/manga/${chapter.manga.id}`}
+          className="font-bold line-clamp-2 break-all"
+        >
+          {chapter.manga.title}
+        </Link>
+        <Link
+          href={`/chapter/${chapter.id}`}
+          className="items-center flex gap-1"
+        >
           {chapter.language === "vi" ? (
             <VN className="size-4 shrink-0" />
           ) : (
             <GB className="size-4 shrink-0" />
           )}
-          <span className="font-bold text-sm line-clamp-1 break-all hover:underline">{title}</span>
+          <span className="font-bold text-sm line-clamp-1 break-all hover:underline">
+            {title}
+          </span>
         </Link>
       </CardFooter>
     </Card>
