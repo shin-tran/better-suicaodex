824096a2d2197c63cf66cfc5c77b0e5e5488c3db
iamneyk <iamneyk@gmail.com>
2025-03-24 00:52:13 +0700

    extra debug for stuff module

--- src/components/Pages/Home/StaffPick/index.tsx
+++ src/components/Pages/Home/StaffPick/index.tsx
@@ -59,7 +59,7 @@ export default function StaffPick() {
     return (
       <div className="flex flex-col">
         <hr className="w-9 h-1 bg-primary border-none" />
-        <h1 className="text-2xl font-black uppercase">Truyện mới</h1>
+        <h1 className="text-2xl font-black uppercase">Truyện đề cử</h1>
 
         <div className="mt-4 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3">
           {[...Array(8)].map((_, index) => (
