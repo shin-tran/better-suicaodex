a73781628e7a2bfe775eecd6e8133144a9c72956
iamneyk <iamneyk@gmail.com>
2025-03-27 22:16:10 +0700

    somebody keeps erasing my changes.

---
+++ src/app/author/[id]/page.tsx
@@ -0,0 +1,10 @@
+interface PageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
+  return <div>mẹ mày {id}</div>;
+}
