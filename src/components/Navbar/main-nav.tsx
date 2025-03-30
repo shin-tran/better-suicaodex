2a1ea072b9727ccb8b29019bccc1332676435efc
iamneyk <iamneyk@gmail.com>
2025-03-30 20:54:35 +0700

    mỏe logo 😭😭😭😭

--- src/components/Navbar/main-nav.tsx
+++ src/components/Navbar/main-nav.tsx
@@ -5,106 +5,60 @@ import useScrollOffset from "@/hooks/use-scroll-offset";
 import { cn } from "@/lib/utils";
 // import { useIsMobile } from "@/hooks/use-mobile";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
+import { getLogo, logos } from "../logos";
+import { useState, useEffect } from "react";
 
 export function MainNav() {
   const { isAtTop } = useScrollOffset();
   // const isMobile = useIsMobile();
   const pathname = usePathname();
+  const [logoSrc, setLogoSrc] = useState<any>(null);
+
+  useEffect(() => {
+    const logosName = [
+      "gehenna",
+      // "redwinter",
+      "shanhaijing",
+      "srt",
+      "trinity",
+      "valkyrie",
+      "abydos",
+      "arius",
+      "millennium",
+    ];
+    const randomLogo = logosName[Math.floor(Math.random() * logosName.length)];
+    setLogoSrc(getLogo(randomLogo));
+  }, []);
 
   return (
-    <div>
-      <Link
-        href="/"
-        className="mr-4 flex items-center gap-1 justify-start lg:mr-6"
-      >
-        {/* <Icons.logo className="h-6 w-6" /> */}
-        <img
-          src="/images/cunny.webp"
-          fetchPriority="high"
-          alt="uhhohhh"
-          className="h-7 w-auto"
-        />
-        <img
-          src="/suicaodex.webp"
-          fetchPriority="high"
-          alt="SuicaoDex"
-          className={cn(
-            "h-[22px] w-auto drop-shadow-md dark:invert hidden xs:flex",
-            pathname.includes("/manga") && "md:invert",
-            // pathname.includes("/group/") && "md:invert",
-            !isAtTop && "filter-none md:filter-none"
-          )}
-        />
-        {/* <span  className="font-bold inline-block">{siteConfig.name}</span> */}
-      </Link>
-      {/* <nav className="flex items-center gap-4 text-sm xl:gap-6">
-        <Link
-          href="/docs"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname === "/docs" ? "text-foreground" : "text-foreground/80"
-          )}
-        >
-          Docs
-        </Link>
-        <Link
-          href="/docs/components"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/docs/components") &&
-              !pathname?.startsWith("/docs/component/chart")
-              ? "text-foreground"
-              : "text-foreground/80"
-          )}
-        >
-          Components
-        </Link>
-        <Link
-          href="/blocks"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/blocks")
-              ? "text-foreground"
-              : "text-foreground/80"
-          )}
-        >
-          Blocks
-        </Link>
-        <Link
-          href="/charts"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/docs/component/chart") ||
-              pathname?.startsWith("/charts")
-              ? "text-foreground"
-              : "text-foreground/80"
-          )}
-        >
-          Charts
-        </Link>
-        <Link
-          href="/themes"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/themes")
-              ? "text-foreground"
-              : "text-foreground/80"
-          )}
-        >
-          Themes
-        </Link>
-        <Link
-          href="/colors"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/colors")
-              ? "text-foreground"
-              : "text-foreground/80"
-          )}
-        >
-          Colors
-        </Link>
-      </nav> */}
-    </div>
+    <Link
+      href="/"
+      className="mr-4 flex items-center gap-0.5 justify-start lg:mr-6"
+    >
+      <Image
+        src={logoSrc || logos.trinity}
+        alt="SuicaoDex's logo"
+        className={cn(
+          "h-8 w-auto grayscale contrast-150 dark:invert",
+          pathname.includes("/manga") && "md:invert",
+          !isAtTop && "invert-0 md:invert-0"
+        )}
+        quality={100}
+        priority
+      />
+      <Image
+        src={logos.suicaodex}
+        alt="SuicaoDex's logo"
+        quality={100}
+        priority
+        className={cn(
+          "h-[22px] w-auto drop-shadow-md dark:invert hidden xs:flex",
+          pathname.includes("/manga") && "md:invert",
+          // pathname.includes("/group/") && "md:invert",
+          !isAtTop && "filter-none md:filter-none"
+        )}
+      />
+    </Link>
   );
 }
