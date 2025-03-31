f735b16d04b21067ab83e457020c930c1e7b5248
iamneyk <iamneyk@gmail.com>
2025-03-31 17:12:21 +0700

    Abandon all hope, ye who enter here.

--- src/components/Chapter/ChapterReader/Reader/long-strip.tsx
+++ src/components/Chapter/ChapterReader/Reader/long-strip.tsx
@@ -10,12 +10,20 @@ interface LongStripProps {
 }
 
 export default function LongStrip({ images }: LongStripProps) {
-  const [loaded, setLoaded] = useState(false);
+  // const [loaded, setLoaded] = useState(false);
+
   const [config] = useConfig();
+
+  const [loadedCount, setLoadedCount] = useState(0);
+  const allLoaded = loadedCount === images.length;
+
   return (
-    <div className={cn("min-w-0 relative mt-2",
-      loaded ? "min-h-0" : "min-h-lvh" 
-    )}>
+    <div
+      className={cn(
+        "min-w-0 relative mt-2",
+        allLoaded ? "min-h-0" : "min-h-lvh"
+      )}
+    >
       <div
         className={cn(
           "overflow-x-auto flex flex-col items-center h-full select-none bg-transparent"
@@ -27,9 +35,13 @@ export default function LongStrip({ images }: LongStripProps) {
         {images.map((image, index) => (
           <span
             key={index + 1}
-            className={`block overflow-hidden ${
-              loaded ? "min-h-0" : "min-h-[100vh]"
-            }`}
+            // className={`block overflow-hidden ${
+            //   loaded ? "min-h-0" : "min-h-[100vh]"
+            // }`}
+            className="block overflow-hidden"
+            style={{
+              minHeight: allLoaded ? "auto" : "500px",
+            }}
           >
             <LazyLoadImage
               wrapperClassName="!block"
@@ -38,7 +50,8 @@ export default function LongStrip({ images }: LongStripProps) {
                 "h-auto w-auto mx-auto",
                 config.reader.imageFit === "height" && "!max-h-screen"
               )}
-              onLoad={() => setLoaded(true)}
+              // onLoad={() => setLoaded(true)}
+              onLoad={() => setLoadedCount((prev) => prev + 1)}
               onError={(e) => {
                 e.currentTarget.src = "/images/xidoco.webp";
               }}
