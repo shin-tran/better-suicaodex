f0226c2058e37f498d58794445ec5309687b1c39
iamneyk <iamneyk@gmail.com>
2025-04-09 00:38:46 +0700

    Who has two thumbs and remembers the rudiments of his linear algebra courses?  Apparently, this guy.

--- src/lib/rss-fetcher.ts
+++ src/lib/rss-fetcher.ts
@@ -1,25 +1,25 @@
-const RSS_URL = "https://mdrss.tijlvdb.me/feed";
-const SCD_RSS_URL = "https://cors.iamneyk.workers.dev/?url=https://git.suicaodex.com/rss";
-import Parser from 'rss-parser';
+// const RSS_URL = "https://mdrss.tijlvdb.me/feed";
+// const SCD_RSS_URL = "https://cors.iamneyk.workers.dev/?url=https://git.suicaodex.com/rss";
+// import Parser from 'rss-parser';
 
-export async function fetchRssFeedByIds(ids:string[]) {
-    if (ids.length === 0) return null;
+// export async function fetchRssFeedByIds(ids:string[]) {
+//     if (ids.length === 0) return null;
 
-    const params = ids.map(id => `q=manga:${id},tl:vi`).join("&");
-    const url = `${RSS_URL}?${params}`;
-    console.log("url", url);
+//     const params = ids.map(id => `q=manga:${id},tl:vi`).join("&");
+//     const url = `${RSS_URL}?${params}`;
+//     console.log("url", url);
 
-    const parser = new Parser();
-    const feed = await parser.parseURL(url);
-    // console.log("feed", feed.items);
+//     const parser = new Parser();
+//     const feed = await parser.parseURL(url);
+//     // console.log("feed", feed.items);
     
-    return feed.items;
-}
+//     return feed.items;
+// }
 
-export async function fetchRssFeed(){
-    const parser = new Parser();
-    const feed = await parser.parseURL(SCD_RSS_URL);
-    // console.log("items", feed.items);
+// export async function fetchRssFeed(){
+//     const parser = new Parser();
+//     const feed = await parser.parseURL(SCD_RSS_URL);
+//     // console.log("items", feed.items);
     
-    return feed.items;
-}
+//     return feed.items;
+// }
