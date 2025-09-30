b159d06165bac5c9d6a543cf760f139bee0b57ad
TNTKien <95180188+TNTKien@users.noreply.github.com>
2025-09-30 21:26:55 +0700

    hẹ hẹ hẹ

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -17,6 +17,7 @@ import { setCurrentApiUrl } from "./utils";
 
 // Danh sách proxy
 const proxyList = [
+  "https://proxy.42015881.workers.dev",
   "https://api.suicaodex.com",
   "https://proxy.baclethanxa2022.workers.dev",
   "https://api2.suicaodex.com",
